ee3dc41f0049c5f7f829e11b4d4d5792a010ff56
Devin Civiello <devin.m.civiello@gmail.com>
2026-04-01 15:18:37 -0400

    Changed description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,6 @@
 <h1 class="title">Devin Civiello</h1>
 
-<p class="description">
-	Thanks for visiting my website! This will be a place to share things that I like. I'm doing all
-	the styling by hand (no AI) to both improve my CSS skills and so that hopefully the end product is
-	a more unique expression of my personality.
-</p>
+<p class="description">Romans 1:16</p>
 
 <div class="media-section">
 	<div class="media-container">
@@ -26,6 +22,7 @@
 		text-align: center;
 	}
 	.description {
+		text-align: center;
 		font-size: 1.2rem;
 		color: #000;
 		margin: 2rem;
